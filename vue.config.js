66df5abb9ef0b67752a2777df40a47842139619e
wenlun <1070586554@qq.com>
2020-06-18 10:28:28 +0800

    框架改动

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    publicPath: '/vue-luckdraw-demo/',
     // 修改 src 目录 为 examples 目录
     pages: {
         index: {
@@ -9,6 +10,7 @@ module.exports = {
             filename: 'index.html',
         },
     },
+    productionSourceMap: false,
     // 扩展 webpack 配置，使 packages 加入编译
     chainWebpack: config => {
         config.module
